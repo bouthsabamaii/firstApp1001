fb403f69fbf3c00218a1438f0b7abf73d2839d49
bouthsabamaii <92769118+bouthsabamaii@users.noreply.github.com>
2022-01-16 11:50:32 +0700

    Create notfound.vue

---
+++ notfound.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1 class="text-center mt-4"> ບໍ່ມີໜ້າທີ່ ທ່ານຄົ້ນຫາ!! </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Myapp2Notfound',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
